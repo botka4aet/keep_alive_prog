99f0e928cd0172d1095025658c3668f3133e1716
botka4aet <shram1992@gmail.com>
2024-08-02 17:34:22 +0300

    conf + routine
    Загрузка настроек из файла и отслеживание нескольких процессов

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"time"
@@ -9,28 +11,78 @@ import (
 	"github.com/shirou/gopsutil/v3/process"
 )
 
-func main() {
-	dir_name := ""
-	exe_name := ""
-	update_name := ""
+type kap_cfg struct {
+	Tick_time    int         `json:"tick_time"`
+	Coma_time    float64     `json:"coma_time"`
+	Log_time     float64     `json:"log_time"`
+	Restart_time float64     `json:"restart_time"`
+	Directories  []Directory `json:"Directories"`
+}
 
+type Directory struct {
+	Cfg_name     string  `json:"cfg_name"`
+	Tick_time    int     `json:"tick_time"`
+	Coma_time    float64 `json:"coma_time"`
+	Log_time     float64 `json:"log_time"`
+	Restart_time float64 `json:"restart_time"`
+	Exe_name     string  `json:"exe_name"`
+	Dir_name     string  `json:"dir_name"`
+	Update_fname string  `json:"update_fname"`
+	Update_name  string  `json:"update_name"`
+}
+
+func main() {
+	jsonFile, err := os.Open("kap_cfg.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := io.ReadAll(jsonFile)
+	var config kap_cfg
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &config)
+
+	for _, dir := range config.Directories {
+		if dir.Dir_name == "" || dir.Exe_name == "" {
+			continue
+		}
+		if dir.Tick_time == 0 {
+			dir.Tick_time = max(config.Tick_time, 1)
+		}
+		if dir.Coma_time == 0 {
+			dir.Coma_time = config.Coma_time
+		}
+		if dir.Log_time == 0 {
+			dir.Log_time = config.Log_time
+		}
+		if dir.Restart_time == 0 {
+			dir.Restart_time = config.Restart_time
+		}
+
+		fmt.Println(logtime(), "Starting...", dir.Cfg_name)
+		go kap_routine(dir)
+	}
+}
+
+func kap_routine(dir Directory) {
 	var count uint8
 	var pid int32
 	var stimec bool
 	var restart bool
+	ticktime := time.Second * time.Duration(dir.Tick_time)
 	dtime := time.Now()
-	fmt.Println(logtime(), "Starting...")
-
 	for {
-		time.Sleep(60 * time.Second)
+		time.Sleep(ticktime)
 		if !stimec {
-			if time.Since(dtime).Minutes() < 45 {
+			if time.Since(dtime).Seconds() < dir.Coma_time {
 				continue
 			}
 			stimec = true
 		}
 		count++
-		entries, err := os.ReadDir(update_name)
+		entries, err := os.ReadDir(dir.Update_name)
 		if err != nil {
 			logerror(err)
 			continue
@@ -45,27 +97,27 @@ func main() {
 			if modtime.Before(dtime) {
 				dtime = modtime
 			}
-			tsince := time.Since(modtime).Minutes()
-			if pid != 0 && tsince > 60 && !restart {
+			tsince := time.Since(modtime).Seconds()
+			if pid != 0 && tsince > dir.Restart_time && !restart {
 				fmt.Println(logtime(), "Restarting for old file -", e.Name())
 				restart = true
 				break
-			} else if tsince > 45 {
-				fmt.Println(logtime(), "Outdated file -", e.Name(), "-", tsince, "min")
+			} else if tsince > dir.Log_time {
+				fmt.Println(logtime(), "Outdated file -", e.Name(), "-", tsince/60, "min")
 			}
 		}
 
 		processes, _ := process.Processes()
 		for _, p := range processes {
 			n, _ := p.Name()
-			if n != exe_name {
+			if n != dir.Exe_name {
 				continue
 			}
 			l, err := p.Exe()
 			if err != nil {
 				logerror(err)
 				continue
-			} else if l != dir_name+exe_name {
+			} else if l != dir.Dir_name+n {
 				continue
 			}
 			nid, err := p.Ppid()
@@ -90,14 +142,14 @@ func main() {
 		if restart {
 			time.Sleep(5 * time.Second)
 			fmt.Println(logtime(), "Starting new process...")
-			cmd := exec.Command("cmd.exe", "/C", "start", dir_name+exe_name)
-			cmd.Dir = dir_name
+			cmd := exec.Command("cmd.exe", "/C", "start", dir.Dir_name+dir.Exe_name)
+			cmd.Dir = dir.Dir_name
 			err := cmd.Run()
 			if err != nil {
 				logerror(err)
 			}
 		}
-		if count > 2 {
+		if count > 5 {
 			restart = true
 		}
 	}
