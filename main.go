13f0002985f759d2b1c503e3652f5998584d5516
botka4aet <shram1992@gmail.com>
2024-08-06 17:29:24 +0300

    Вывод в консоль
    Передача в общий канал для дальнейшего логирования

--- main.go
+++ main.go
@@ -20,34 +20,52 @@ type kap_cfg struct {
 }
 
 type Directory struct {
-	Cfg_name     string  `json:"cfg_name"`
 	Tick_time    int     `json:"tick_time"`
 	Coma_time    float64 `json:"coma_time"`
+	Dir_name     string `json:"dir_name"`
+	Update_fname string `json:"update_fname"`
+	Update_name  string `json:"update_name"`
+
+	Tick_time int     `json:"tick_time"`
+	Coma_time float64 `json:"coma_time"`
+
 	Log_time     float64 `json:"log_time"`
-	Log_type     bool    `json:"log_type"`
 	Restart_time float64 `json:"restart_time"`
-	Exe_name     string  `json:"exe_name"`
-	Dir_name     string  `json:"dir_name"`
-	Update_fname string  `json:"update_fname"`
-	Update_name  string  `json:"update_name"`
+
+	Log_type bool `json:"log_type"`
+}
+
+func init() {
+	err := os.Remove("KAP_logs.log")
+	if err != nil && !os.IsNotExist(err) {
+		fmt.Println(logtime("[MAIN]"),err)
+	}
 }
 
 func main() {
 	jsonFile, err := os.Open("kap_cfg.json")
 	if err != nil {
-		logerror(err,"[MAIN]")
+		fmt.Println(logtime("MAIN"),err)
 	} else {
-		fmt.Println(logtime("[MAIN]"), "Starting MAIN process...")
+		fmt.Println(logtime("MAIN"), "Starting MAIN process...")
 	}
 	defer jsonFile.Close()
-	byteValue, _ := io.ReadAll(jsonFile)
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(logtime("MAIN"),err)
+	}
 	var config kap_cfg
-	json.Unmarshal(byteValue, &config)
-
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		fmt.Println(logtime("MAIN"),err)
+	}
+	ch := make(chan string)
+	var i int
 	for _, dir := range config.Directories {
 		if dir.Dir_name == "" || dir.Exe_name == "" {
 			continue
 		}
+		i++
 		if dir.Tick_time == 0 {
 			dir.Tick_time = max(config.Tick_time, 1)
 		}
@@ -60,16 +78,20 @@ func main() {
 		if dir.Restart_time == 0 {
 			dir.Restart_time = config.Restart_time
 		}
+		if dir.Cfg_name == "" {
+			dir.Cfg_name = strconv.Itoa(i)
+		}
 
 		fmt.Println(logtime(dir.Cfg_name), "Starting...")
-		go kap_routine(dir)
+		go kap_routine(dir,ch)
 	}
-	for {
-		time.Sleep(3600)
+	for i > 0 {
+		mes := <-ch
+		fmt.Print(mes)
 	}
 }
 
-func kap_routine(dir Directory) {
+func kap_routine(dir Directory,ch chan<- string) {
 	var count uint8
 	var pid int32
 	var stimec bool
@@ -99,22 +121,22 @@ func kap_routine(dir Directory) {
 			if check_type {
 				fileinfo, err := os.Stat(dir.Update_fname)
 				if err != nil {
-					logerror(err,dir.Cfg_name)
+					ch <- logerror(err, dir.Cfg_name)
 					continue
 				}
 				modtime := fileinfo.ModTime()
 				tsince := time.Since(modtime).Seconds()
 				if pid != 0 && tsince > dir.Restart_time && !restart {
-					restart_reason = fmt.Sprint(logtime(dir.Cfg_name), " Restarting for old file", fileinfo.Name(), "\n")
+					restart_reason = fmt.Sprintf("%v Restarting for old file - %v\n", logtime(dir.Cfg_name), fileinfo.Name())
 					restart = true
 				} else if tsince > dir.Log_time {
-					check_result = fmt.Sprintf("%vOutdated file - %v - %.2f min\n", logtime(dir.Cfg_name),fileinfo.Name(), tsince/60)
+					check_result = fmt.Sprintf("%v Outdated file - %v - %.2f min\n", logtime(dir.Cfg_name), fileinfo.Name(), tsince/60)
 				}
-			//Проверяем папку лог-файлов
+				//Проверяем папку лог-файлов
 			} else {
 				entries, err := os.ReadDir(dir.Update_name)
 				if err != nil {
-					logerror(err,dir.Cfg_name)
+					ch <- logerror(err, dir.Cfg_name)
 					continue
 				}
 				for _, e := range entries {
@@ -125,22 +147,22 @@ func kap_routine(dir Directory) {
 					modtime := fileinfo.ModTime()
 
 					tsince := time.Since(modtime).Seconds()
-					if pid != 0 && tsince > dir.Restart_time && !restart {
-						restart_reason = fmt.Sprint(logtime(dir.Cfg_name), "Restarting for old file", fileinfo.Name(), "\n")
+					if tsince > dir.Restart_time && !restart {
+						restart_reason = fmt.Sprintf("%v Restarting for old file - %v\n", logtime(dir.Cfg_name), fileinfo.Name())
 						restart = true
 					} else if tsince > dir.Log_time && tsince < dir.Restart_time {
 						if dir.Log_type {
-							check_result = check_result + fmt.Sprintf("%vOutdated file - %v - %.2f min\n", logtime(dir.Cfg_name),fileinfo.Name(), tsince/60)
-						} else if !modtime.After(dtime) {
+							check_result = check_result + fmt.Sprintf("%v Outdated file - %v - %.2f min\n", logtime(dir.Cfg_name), fileinfo.Name(), tsince/60)
+						} else if !modtime.After(dtime) || check_result == "" {
 							dtime = modtime
-							check_result = fmt.Sprintf("%vOutdated file - %v - %.2f min\n", logtime(dir.Cfg_name),fileinfo.Name(), tsince/60)
+							check_result = fmt.Sprintf("%v Outdated file - %v - %.2f min\n", logtime(dir.Cfg_name), fileinfo.Name(), tsince/60)
 						}
 					}
 				}
 			}
 		}
 		if check_result != "" {
-			fmt.Print(check_result)
+			ch <- check_result
 		}
 		if count > 5 {
 			restart_reason = fmt.Sprint(logtime(dir.Cfg_name), " Restarting for errors\n")
@@ -155,14 +177,14 @@ func kap_routine(dir Directory) {
 			}
 			l, err := p.Exe()
 			if err != nil {
-				logerror(err,dir.Cfg_name)
+				ch <- logerror(err, dir.Cfg_name)
 				continue
 			} else if l != dir.Dir_name+n {
 				continue
 			}
 			nid, err := p.Ppid()
 			if err != nil {
-				logerror(err,dir.Cfg_name)
+				ch <- logerror(err, dir.Cfg_name)
 				continue
 			}
 			if nid != pid {
@@ -174,30 +196,34 @@ func kap_routine(dir Directory) {
 				restart = false
 			}
 			if restart {
-				fmt.Print(restart_reason)
-				fmt.Println(logtime(dir.Cfg_name), "Killing old process...")
-				p.Kill()
+				ch <- fmt.Sprint(restart_reason)
+				ch <- fmt.Sprintln(logtime(dir.Cfg_name), "Killing old process...")
+				err := p.Kill()
+				if err != nil {
+					ch <- logerror(err, dir.Cfg_name)
+					continue
+				}
 				pid = 0
 			}
 			count = 0
 		}
 		if restart {
 			time.Sleep(5 * time.Second)
-			fmt.Println(logtime(dir.Cfg_name), "Starting new process...")
+			ch <- fmt.Sprintln(logtime(dir.Cfg_name), "Starting new process...")
 			cmd := exec.Command("cmd.exe", "/C", "start", dir.Dir_name+dir.Exe_name)
 			cmd.Dir = dir.Dir_name
 			err := cmd.Run()
 			if err != nil {
-				logerror(err,dir.Cfg_name)
+				ch <- logerror(err, dir.Cfg_name)
 			}
 		}
 	}
 }
 
 func logtime(cfg_name string) string {
-	return fmt.Sprint("["+cfg_name+"]",time.Now().Format("[15:04:05]"))
+	return fmt.Sprint("["+cfg_name+"]", time.Now().Format("[15:04:05]"))
 }
 
-func logerror(err error,cfg_name string) {
-	fmt.Println(logtime(cfg_name), err)
+func logerror(err error, cfg_name string) string {
+	return fmt.Sprintln(logtime(cfg_name), err)
 }
